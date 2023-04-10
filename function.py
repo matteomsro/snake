cfd5bdf3556097aabfb6fc2bee388072c09c53fd
zab <thomas.zabera@ensta-bretagne.org>
2023-04-10 17:24:49 -0500

    modif

--- function.py
+++ function.py
@@ -3,120 +3,63 @@ from PIL import Image, ImageOps
 import numpy as np
 
 
-
 def find_path(matrix, start, end):
-    if end==None:
+    if end == None:
         return None
-    moves=[]
+    
     # Créer une file vide et ajouter le point de départ
-    q = Queue()
-    q.put(start)
-
+    q = [start]
+    
     # Créer un dictionnaire pour enregistrer le chemin parcouru
     path = {start: None}
-
-    while not q.empty():
-        current = q.get()
-
+    
+    while q:
+        current = q.pop(0)
+        
         # Si on a atteint le point final, construire la liste de mouvements
         if current == end:
             moves = []
             while current != start:
                 parent = path[current]
-                if parent[0] < current[0]:
-                    moves.append('bas')
-                elif parent[0] > current[0]:
+                diff = tuple(a - b for a, b in zip(current, parent))
+                if diff == (-1, 0):
                     moves.append('haut')
-                elif parent[1] < current[1]:
-                    moves.append('droite')
-                elif parent[1] > current[1]:
+                elif diff == (1, 0):
+                    moves.append('bas')
+                elif diff == (0, -1):
                     moves.append('gauche')
+                elif diff == (0, 1):
+                    moves.append('droite')
                 current = parent
             return moves[::-1]
-
+        
         # Visiter les voisins du point actuel
-        for neighbor in get_neighbors(matrix, current,end):
+        for neighbor in get_neighbors(matrix, current, end):
             if neighbor not in path:
-                q.put(neighbor)
+                q.append(neighbor)
                 path[neighbor] = current
+    
+    # Si aucun chemin n'a été trouvé
+    return None
+
+
 
 def get_neighbors(matrix, point, end):
     # Récupérer les coordonnées des voisins valides
     neighbors = []
     rows, cols = matrix.shape
     row, col = point
-    if (row - 1 >= 0) and (matrix[row - 1, col] != 1):
+    if (row - 1 >= 0) and (col<cols) and (matrix[row - 1, col] != 1):
         neighbors.append((row - 1, col))
-    if (row + 1 < rows) and (matrix[row + 1, col] != 1):
+    if (row + 1 < rows) and (col<cols) and (matrix[row + 1, col] != 1):
         neighbors.append((row + 1, col))
-    if (col - 1 >= 0) and (matrix[row, col - 1] != 1):
+    if (col - 1 >= 0) and (row<rows) and (matrix[row, col - 1] != 1):
         neighbors.append((row, col - 1))
-    if (col + 1 < cols) and (matrix[row, col + 1] != 1):
+    if (col + 1 < cols) and (row<rows) and (matrix[row, col + 1] != 1):
         neighbors.append((row, col + 1))
-
-    # Ajouter la case adjacente au mur si c'est la seule option pour atteindre la pomme
-    if (end in neighbors) and (len(neighbors) == 1):
-        wall_neighbors = []
-        if (row - 1 >= 0) and (matrix[row - 1, col] == 1):
-            wall_neighbors.append((row - 1, col))
-        if (row + 1 < rows) and (matrix[row + 1, col] == 1):
-            wall_neighbors.append((row + 1, col))
-        if (col - 1 >= 0) and (matrix[row, col - 1] == 1):
-            wall_neighbors.append((row, col - 1))
-        if (col + 1 < cols) and (matrix[row, col + 1] == 1):
-            wall_neighbors.append((row, col + 1))
-        neighbors.extend(wall_neighbors)
 
     return neighbors
 
 
-def find_shortest_path(image_path):
-    # Ouvrir l'image en niveau de gris et inverser les couleurs
-    image = ImageOps.invert(Image.open(image_path).convert('L'))
-    # Convertir l'image en un tableau numpy
-    matrix = np.array(image)
-    # Ajouter une bordure de murs autour de la matrice
-    surrounded = surround_matrix(matrix)
-    # Trouver les points de départ et d'arrivée
-    start = np.argwhere(matrix == 0)[0]
-    end = np.argwhere(matrix == 255)[-1]
-    # Trouver le chemin le plus court
-    path = find_path(surrounded, tuple(start + 1), tuple(end + 1))
-    
-    # Si le chemin est nul, il y a une erreur, renvoyer une liste vide
-    if path is None:
-        return []
-    
-    # Si la longueur du chemin est inférieure à 2, retourner le chemin tel quel
-    if len(path) < 2:
-        return path
-    
-    # Vérifier si le serpent se dirige vers le mur
-    if path[0] == 'bas' and matrix[start[0]+1][start[1]] == 1:
-        # Si le serpent va vers le bas et rencontre un mur, essayer de tourner à gauche ou à droite
-        if matrix[start[0]][start[1]-1] != 1:
-            return ['gauche'] + path
-        elif matrix[start[0]][start[1]+1] != 1:
-            return ['droite'] + path
-    elif path[0] == 'haut' and matrix[start[0]-1][start[1]] == 1:
-        # Si le serpent va vers le haut et rencontre un mur, essayer de tourner à gauche ou à droite
-        if matrix[start[0]][start[1]-1] != 1:
-            return ['gauche'] + path
-        elif matrix[start[0]][start[1]+1] != 1:
-            return ['droite'] + path
-    elif path[0] == 'droite' and matrix[start[0]][start[1]+1] == 1:
-        # Si le serpent va vers la droite et rencontre un mur, essayer de tourner en haut ou en bas
-        if matrix[start[0]-1][start[1]] != 1:
-            return ['haut'] + path
-        elif matrix[start[0]+1][start[1]] != 1:
-            return ['bas'] + path
-    elif path[0] == 'gauche' and matrix[start[0]][start[1]-1] == 1:
-        # Si le serpent va vers la gauche et rencontre un mur, essayer de tourner en haut ou en bas
-        if matrix[start[0]-1][start[1]] != 1:
-            return ['haut'] + path
-        elif matrix[start[0]+1][start[1]] != 1:
-            return ['bas'] + path
-    
-    # Sinon, retourner le chemin tel quel
-    return path
+
 
--- thefrenchsnake.py
+++ thefrenchsnake.py
@@ -18,8 +18,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def snake():
+    j=4
     liste_avt=[]
     liste_avt2=[]
+    historique=[[0,0]]
 
     dict = {"haut":Keys.UP,"bas":Keys.DOWN,"gauche":Keys.LEFT,"droite":Keys.RIGHT}
     # Créer une instance du navigateur Chrome
@@ -105,12 +107,15 @@ def snake():
             pos_pomme=(round(15*moyenne_rouge[0]/569),round(17*moyenne_rouge[1]/614)+1)
             print("Pixel rouge trouvé à la position",pos_pomme)
 
-        
+        if len(historique)>j:
+            historique.pop(0)
+        if abs(pos_tete[0]-pos_pomme[0])<2 and abs(pos_tete[1]-pos_pomme[1])<2 : j+=1
         tableau = np.zeros((15, 17))
-    
-
+        historique.append(pos_tete)
+        for m in historique:
+            tableau[m[0]-1,m[1]-1]=1
         liste = find_path(tableau,pos_tete,pos_pomme)
-        #print(liste)
+        #print(tableau)
         #liste = list(set(liste))
         lit.append((i,pos_tete,pos_pomme,liste))
         #print(lit)
